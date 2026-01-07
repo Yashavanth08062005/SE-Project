f58ac15427ac1c53670b6ff7d36887ed0e4d06e4
yashavanth <yashavanthkate@gmail.com>
2026-01-07 23:05:30 +0530

    Empty repository

--- Downloads/beckn-main/beckn-main/Downloads/beckn-confirm-master (1)/beckn-confirm-master/bap-travel-discovery/read_logs.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-try {
-    const logs = fs.readFileSync('schema_error.log', 'utf8');
-    console.log(logs);
-} catch (e) {
-    console.error('Error reading log:', e.message);
-}
